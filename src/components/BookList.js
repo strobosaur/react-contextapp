dfc74205910ac51f988d946ab35ef14fb22b186f
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-09-28 14:38:48 +0200

    ThemeContext.Consumer used to access context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,16 +4,20 @@ import { ThemeContext } from '../contexts/ThemeContext';
 class BookList extends React.Component {
   static contextType = ThemeContext;
   render() { 
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
     return (
-      <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
-        <ul>
-          <li style={{ background: theme.ui }}>Blindsight</li>
-          <li style={{ background: theme.ui }}>Blood meridian</li>
-          <li style={{ background: theme.ui }}>Neuromancer</li>
-        </ul>
-      </div>
+      <ThemeContext.Consumer>{(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <div className='book-list' style={{ background: theme.bg, color: theme.syntax }}>
+            <ul>
+              <li style={{ background: theme.ui }}>Blindsight</li>
+              <li style={{ background: theme.ui }}>Blood meridian</li>
+              <li style={{ background: theme.ui }}>Neuromancer</li>
+            </ul>
+          </div>
+        )
+      }}</ThemeContext.Consumer>
     );
   }
 }
